38bfe9301f2708d67c5240a1ad96c874bd3920ee
Fhnx <Fhnx@users.noreply.github.com>
2015-01-28 00:03:40 +0100

    input verification Parser init remove comments remove empty lines

--- src/assembler/Parser.java
+++ src/assembler/Parser.java
@@ -1,5 +1,74 @@
 package assembler;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
 public class Parser {
+	private InputStreamReader inStream;
+	private OutputStreamWriter outStream;
+
+	private String[] code;
+
+	public Parser(InputStream source, OutputStream dest) {
+		this(new InputStreamReader(source), new OutputStreamWriter(dest));
+	}
+
+	public Parser(InputStreamReader source, OutputStreamWriter dest) {
+		inStream = source;
+		outStream = dest;
+		code = null;
+	}
+
+	public int parse() {
+		StringBuilder stringBuf = new StringBuilder();
+		int ch;
+		try {
+			while ((ch = inStream.read()) > -1) {
+				stringBuf.append((char) ch);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return 1;
+		}
+		System.out.println("Source file contents:\n" + stringBuf.toString());
+		System.out.println("-----------------------");
+		
+		code = stringBuf.toString().split("\n");
+		System.out.println("Num lines: " + code.length);
+		int numNonEmpty = 0;
+		for (int idx = 0; idx < code.length; idx++) {
+			String str = code[idx];
+			System.out.println(str);
+			int idxOf = str.indexOf("//");
+			if(idxOf != -1){
+				str = str.substring(0, idxOf);
+				code[idx] = str;
+			}
+			
+			if( str.length() > 1)
+				numNonEmpty++;
+		}
+		int idxTmpArr = 0;
+		String[] tmpArr = new String[numNonEmpty];
+		for (String string : code) {
+			if(string.length() > 1){
+				tmpArr[idxTmpArr] = string;
+				idxTmpArr++;
+			}
+		}
+		code = tmpArr;
+		tmpArr = null;
+		
+		
+		System.out.println("Pure code:");
+		for (String string : code) {
+			System.out.println(string);
+		}
+		System.out.println("-----------------------");
+		return 0;
+	}
 
 }
--- src/ui/Assembler.java
+++ src/ui/Assembler.java
@@ -1,7 +1,86 @@
 package ui;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import assembler.Parser;
+
 public class Assembler {
+	private static String sourceFilePath = null;
+	private static String destFilePath = null;
+
 	public static void main(String[] args) {
-		
+		System.out.println("-----------------------");
+		System.out.println("Number of arguments: " + args.length);
+		System.out.println("The arguments are:");
+		for (String string : args) {
+			System.out.println(string);
+		}
+		int retStat = assignFilePaths(args);
+		if (retStat == 0) {
+			Parser par = null;
+			try {
+				par = new Parser(new FileInputStream(sourceFilePath), new FileOutputStream(destFilePath, false));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				System.out.println("Error: Couldn't open Filestreams!");
+			}
+			if(par != null){
+				retStat = par.parse();
+				if(retStat == 0){
+					
+				}else{
+					System.out.println("Error: While parsing. Error-code: " + retStat);
+				}
+			}
+		} else {
+			System.out.println("Canceled!");
+		}
+		System.out.println("-----------------------");
+	}
+
+	private static int assignFilePaths(String[] args) {
+		int returnStatus = 0;
+
+		//Check number of arguments
+		if (args.length == 0) {
+			System.out.println("Error: No inputs.");
+			returnStatus = 1;
+		} else {
+			// Add source file
+			if (args[0].toLowerCase().endsWith(".asm")) {
+				sourceFilePath = args[0];
+				System.out.println("Source:\t\t" + sourceFilePath);
+				if (args.length > 2) {
+					System.out
+							.println("Warning: Only the first two arguments get used.");
+				}
+
+				// Add dest file
+				if (args.length > 1 && args[1].toLowerCase().endsWith(".hack")) {
+					destFilePath = args[1];
+				} else {
+					if (args.length == 1) {
+						destFilePath = sourceFilePath.substring(0,
+								sourceFilePath.length() - 4) + ".hack";
+					} else {
+						System.out.println("Error: Invalid destination file.");
+						returnStatus = 1;
+					}
+				}
+
+				if (returnStatus == 0) {
+					System.out.println("Destination:\t" + destFilePath);
+					System.out
+							.println("The destination file will be created and/or overwritten.");
+				}
+			} else {
+				System.out.println("Error: Please enter a *.asm file.");
+				returnStatus = 1;
+			}
+		}
+
+		return returnStatus;
 	}
 }
